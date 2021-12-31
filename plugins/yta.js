1519169e2d0760afa5d4b9391809ceb5590472a4
adulalhy <46670590+adulalhy@users.noreply.github.com>
2021-12-31 07:52:03 +0700

    FIX UPLOAD FILES MORE THAN 10MB

--- plugins/yta.js
+++ plugins/yta.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) =
 *Judul:* ${title}
 *Ukuran File:* ${filesizeF}
 `.trim(), m, null, {
-    asDocument: chat.useDocument, mimetype: 'audio/mp4'
+    asDocument: chat.useDocument, mimetype: 'audio/mp4', upload: conn.waUploadToServer
   })
 }
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
